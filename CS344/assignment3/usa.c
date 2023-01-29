c39c4ac5886331d395f9c621096ab0c7c92b4983
berginp <berginp@oregonstate.edu>
2023-01-29 15:57:50 -0800

    commit

--- CS344/assignment3/usa.c
+++ CS344/assignment3/usa.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,5 +6,47 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]){
-  return 0;
+  while (1){
+    char s[2048];
+    char *words[128];
+
+    printf("Program to run: ");
+
+    fflush(stdout);
+
+    fgets(s, sizeof s, stdin);
+
+    if ((words[0] = strtok(s, " \t\n\r")) != NULL ) {
+      int i = 0;
+      do {
+        //printf("Token: \"%s\"\n", words[i]);
+        i ++;
+      } while ((words[i] = strtok(NULL, " \t\n\r")) != NULL);
+    }
+
+    //execvp(words[0], words);
+
+    if (strcmp(words[0], "cd") == 0){
+      //if (words[1] == NULL){
+      //  chdir(getenv("HOME"));
+      //}
+      if (chdir(words[1]) == -1) {
+        perror("failed to cd");
+      }
+
+    } else if (strcmp(words[0], "exit") == 0){
+      exit(0);
+    } else {
+
+      pid_t pid = fork();
+
+      if (pid == 0){
+        execvp(words[0], words);
+        perror("failed to execvp");
+        exit(1);
+      } else {
+        wait(NULL);
+      }
+    }
+  }
 }

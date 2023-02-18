efe360546846c9511f77bcf7ca65b4657dac46fb
berginp <berginp@oregonstate.edu>
2023-02-18 15:45:32 -0800

    commit

--- CS344/project6/myalloc.c
+++ CS344/project6/myalloc.c
@@ -1,9 +1,32 @@
+#include <unistd.h>
+#include <sys/mman.h>
+
 #define ALIGNMENT 16   // Must be power of 2
 #define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
 
 #define PADDED_SIZE(x) ((x) + GET_PAD(x))
 
 #define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
+
+//global head
+struct block *head = NULL;
+
+struct block {
+    struct block *next;
+    int size;     // Bytes
+    int in_use;   // Boolean
+};
+
+void* myalloc(int size){
+  if (head == NULL){
+    head = mmap(NULL, 1024, PROT_READ|PROT_WRITE,
+                MAP_ANON|MAP_PRIVATE, -1, 0);
+    head->next = NULL;
+    head->size = 1024 - PADDED_SIZE(sizeof(struct block));
+    head->in_use = 0;
+  }
+  return NULL;
+}
 
 int main(void){
   return 0;
